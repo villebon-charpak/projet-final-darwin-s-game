8f756fd4e2f7c03f35e7121355e37489f0d83a44
PistouleKiki <lou.vallognes@u-psud.fr>
2018-05-07 15:21:59 +0200

    Génération Dino
    intermédiaire, ya pas mal de truc que jsuis en train de changer

--- Lou Génération disosaure.py
+++ Lou Génération disosaure.py
@@ -5,20 +5,40 @@ Created on Fri May  4 16:02:19 2018
 
 @author: etudiant
 """
+x = 0
+win = 0
 
-TDino = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+TDino = [0,1,0,0,1,0,0,1,0,0,0,1,0,0,1,0,0,1,0,1]
 TMap = [0,1,0,0,1,0,0,1,0,0,0,1,0,0,1,0,0,1,0,1]
 
-def GenDino():
-    for i in range(20):
+Nbcase = len(TMap)
+"""def GenNewDino(TDino,x):
+    for i in range(x,20):
         TDino[i]= randint(0,1)
         a = TDino[i]
         print(a)
-GenDino()
+    return(TDino)"""
+        
+def TestDino1(TMap,TDino,Nbcase):
+    for i in range(Nbcase):
+        if TMap[i] == 1:
+            if TDino[i] != 1:
+                print("aie")
+                return(i)
+    if x == 0:
+        return(0)
 
-for i in range(20):
-    if TMap[i] == 1:
-        if TDino[i] != 1:
-            print("aie")
-            print(i)
-            break
+win = TestDino1(TMap,TDino,Nbcase)
+print(win)
+                
+
+
+"""while win==0:
+
+    TDino = GenNewDino(TDino,x)
+    TestDino1()"""
+
+
+
+
+
